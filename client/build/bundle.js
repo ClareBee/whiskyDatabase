54a55bba4a25a5d772f544421e32977818a8fd51
Clare <chblackburne@gmail.com>
2017-12-12 19:24:30 +0000

    edit button added

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -106,11 +106,15 @@ WhiskyView.prototype = {
       var hook = document.getElementById('whiskys');
       var text = document.createElement('p');
       var img = document.createElement('img');
+      var link = document.createElement('form');
+      link.classList = "edit";
+      link.innerHTML = `<form action="edit/whisky._id" method="POST"><button type="submit">edit</button></form>`;
       img.classList = "bottle";
       img.src = "../whisky.jpg";
       text.innerText = whisky.name + ": " + '"' + whisky.type + '"';
       hook.appendChild(text);
       hook.appendChild(img);
+      hook.appendChild(link);
     });
   }
 }
